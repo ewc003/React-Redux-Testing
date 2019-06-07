33c1359fbfe714473dc1f746a40c57d02f3a75fc
eugene <yougenechang@gmail.com>
2019-06-06 17:41:33 -0700

    move to xps

---
+++ src/middlewares/async.js
@@ -0,0 +1,22 @@
+// middleware boilerplate
+// export default ({ dispatch }) => next => action => {
+    // check to see if the action has a promise
+    // on its 'payload' property and if it does,
+    // then wait for it to resolve OT, send the
+    // action on to the next middleware
+
+    // we want to wait for the promise to resolve
+    // (gets its data) then creates a new action
+    // with that data and dispatches it
+// }
+export default ({ dispatch }) => next => action => {
+    debugger;
+    if (!action.payload || !action.payload.then) {
+        return next(action);
+    }
+
+    action.payload.then(function(response) {
+        const newAction = { ...action, payload: response };
+        dispatch(newAction);
+    });
+};
